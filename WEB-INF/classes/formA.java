381b593102eb333782a4e9f418460e9ae731fd93
aristotelis96 <aristotelis96@users.noreply.github.com>
2018-03-20 15:15:01 +0200

    formC
    needs fix session not working properly

--- WEB-INF/classes/formA.java
+++ WEB-INF/classes/formA.java
@@ -9,10 +9,6 @@ public class formA extends HttpServlet {
       throws ServletException, IOException
    {
       PrintWriter output;
-      /*(String conti = request.getParameter( "continent" );
-
-      HttpSession session  = request.getSession(true);
-      session.putValue(conti, getCities( conti ));*/
       
       response.setContentType( "text/html" );
       output = response.getWriter();         
@@ -25,17 +21,22 @@ public class formA extends HttpServlet {
 	  output.println( "font-style:    italic; color: red;background-color : black; text-align: center;" );
 	  output.println( "}.form-radio-item label, .form-checkbox-item label {white-space: normal; display: block; left: 14px; top: -14px; position relative; } </style>" );
 	  output.println( "<BODY></HEAD><h1 class='header' text-align='center'>Let's begin!</h1><br><div align='center'>" );
-	  output.println( "<FORM ACTION='http://localhost:8080/mywebsite/formB' method='post'>" );
+	  output.println( "<FORM ACTION='http://localhost:8080/mywebsite/formB' method='get'>" );
 	  output.println( "<h1 style='font-family: TimesNewRoman; color: Green'>First give us some info!</h1>" );
 	  output.println( "First name:<br>" );
 	  output.println( "<input type='text' name='firstname'><br>" );
 	  output.println( "Last name:<br>" );
-	  output.println( "<input type='text' name='lastname'><br><br><br>" );
+	  output.println( "<input type='text' name='lastname'><br>" );
+	  output.println( "Phone number:<br>" );
+	  output.println( "<input type='text' name='phone'><br>" );
+	  output.println( "E-mail:<br>" );
+	  output.println( "<input type='text' name='mail'><br><br><br>" );
 	  output.println( "<h1 style='font-family: TimesNewRoman; color: Green'>Now select a category!</h1>" );
-	  output.println( "<img style=' width: 200px; height: 150px' src='./console.png'>Consoles<input style='margin-right: 50px;' type='radio' name='category' value='console' />" );
+	  output.println( "<img style=' width: 200px; height: 150px' src='./console.png'>Consoles<input style='margin-right: 50px;' type='radio' name='category' value='console' checked='checked' />" );
 	  output.println( "<img style='width: 250px; height: 150px' src='./pc.jpg'>Pc Components<input style='margin-right: 50px;' type='radio' name='category' value='pc' />" );
 	  output.println( "<img style='width: 250px; height: 150px' src='./gameslibrary.png'>Games<input style='margin-right: 50px;' type='radio' name='category' value='Games' />" );
 	  output.println( "<br><br><INPUT TYPE=submit VALUE='Submit'></FORM></div></BODY></HTML>" );
       output.close();    // close stream
    }
+
 }
--- WEB-INF/classes/formC.java
+++ WEB-INF/classes/formC.java
@@ -2,37 +2,31 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class formB extends HttpServlet {
+public class formC extends HttpServlet {
 
-   public void doGet( HttpServletRequest request,
+   public void doPost( HttpServletRequest request,
                        HttpServletResponse response )
       throws ServletException, IOException
    {
       PrintWriter output;
-      String category = request.getParameter( "category" );
-	  String firstname = request.getParameter( "firstname");
-	  String lastname = request.getParameter( "lastname");
-	  String phone = request.getParameter( "phone");
-	  String mail = request.getParameter( "mail");
-
-	  //                             Replace the following 3 'cookie' lines
+      	  //                             Replace the following 3 'cookie' lines
 //Cookie c = new Cookie( conti, getCities( conti ) );
 //c.setMaxAge( 120 );  // seconds until cookie removed
 //response.addCookie( c );  // must precede getWriter
 // BY
       //create a new session (with arg 'true')
-		HttpSession session  = request.getSession(true);
-		session.putValue(category, category);
-		session.putValue(firstname, firstname);
-		session.putValue(lastname, lastname);
-		session.putValue(phone, phone);
-		session.putValue(mail, mail);
-
- //
-      
-      response.setContentType( "text/html" );
-      output = response.getWriter();         
+		HttpSession session  = request.getSession(false);
+		String customerinfo[];
 		
+		if (session!=null)
+			customerinfo = session.getValueNames();
+		else {
+			customerinfo = null;
+		}
+		
+		      
+		response.setContentType( "text/html" );
+		output = response.getWriter();         
 
       // send HTML page to client
       output.println( "<HTML><HEAD><TITLE>E-shop</TITLE> <style>" );
@@ -43,7 +37,57 @@ public class formB extends HttpServlet {
 	  output.println( "font-size:      120px;" );
 	  output.println( "font-style:    italic; color: red;background-color : black; text-align: center;" );
 	  output.println( "}.form-radio-item label, .form-checkbox-item label {white-space: normal; display: block; left: 14px; top: -14px; position relative; } </style>" );
-	  output.println( "<BODY></HEAD><h1 class='header' text-align='center'>Give us info about your order</h1><br><div align='center'>" );
+	  output.println( "<BODY></HEAD><h1 class='header' text-align='center'>Final step! Check your form before submit!</h1><br><div align='center'>" );
+	  
+		if (session!=null){
+			String category = (String)session.getValue(customerinfo[0]);
+			String firstname = (String)session.getValue(customerinfo[1]);
+			String lastname = (String)session.getValue(customerinfo[2]);
+			String phone = (String)session.getValue(customerinfo[3]);
+			String mail = (String)session.getValue(customerinfo[4]);	
+			output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Review your info and submit the form</h1>" );
+			output.print( "<h2>First name:</h2>");
+			output.print( category);		
+			output.print( "<h2>Last name:</h2>");
+			output.print( lastname);
+			output.print( "<h2>Phone number:</h2>");
+			output.print( phone);
+			output.print( "<h2>E-mail:</h2>");
+			output.print( mail);
+			output.print( "<br><br><br><br>");
+			output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Order information:</h1>" );
+			if (category.equals("pc")){
+				String component = request.getParameter( "component" );
+				String message = request.getParameter( "message" );
+				output.print( "<h2>Pc component category:</h2>");
+				output.print( component);		
+				output.print( "<h2>Model:</h2>");
+				output.print( message);		
+				
+			}
+			if (category.equals("games")){
+				
+			}
+			if (category.equals("console")){
+				
+			}
+			output.println( "<FORM ACTION='http://localhost:8080/mywebsite'>" );			
+			output.println( "<INPUT style='background-color: red;' TYPE=submit VALUE='Start over'><br><br>" );
+			output.println( "<FORM ACTION='http://localhost:8080/mywebsite/formC' method='GET'>" );			
+			output.println( "<INPUT style='width: 300px; height: 100px;' TYPE=submit VALUE='Submit'>" );
+
+
+			
+		}
+		else {
+			output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Something went wrong!</h1><br>" );
+			output.println( "<h3 style='font-family: TimesNewRoman; color: red;'>Your session has expired</h3>" );			 
+			output.println( "<FORM ACTION='http://localhost:8080/mywebsite'>" );
+			output.println( "<br><INPUT TYPE=submit VALUE='Start over!'>" );
+
+			
+		}
+		/*
 	  output.println( "<FORM ACTION='http://localhost:8080/mywebsite/formC' method='post'>" );
 	  if (category.equals("pc")){
 			 output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Lets find the best pc component</h1>" );
@@ -64,21 +108,14 @@ public class formB extends HttpServlet {
 			 output.println( "" );
 	  }	
 	  if (category.equals("Games")){
-			 output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Tell us about the game you want to order</h1><br>" );
-			 output.println( "<h3 style='font-family: TimesNewRoman; color: red;'>Select a gaming category</h3>" );			 
-			 output.println( "  <select style='font-family: TimesNewRoman; font-size: 30px' name='gamecategory'>");
-			 output.println( "<option value='action'>Action</option> ");
-			 output.println( "<option value='adventure'>Adventure</option>  ");			 
-			 output.println( "<option value='roleplaying'>Role-Playing games</option> ");
-			 output.println( "<option value='sports'>Sports</option>  ");
-			 output.println( "<option value='strategy'>Strategy</option>  ");
-			 output.println( "<option value='racing'>Racing</option>  ");
-			 output.println( " </select><br><br><br>");
-			 output.println( "<textarea style='font-family: TimesNewRoman; font-size: 20px;' name='message' rows='10' cols='30'>" );
-			 output.println( "Please name the title of the game..." );
-			 output.println( "</textarea><br><br>" );
-			 output.println( "<INPUT TYPE='reset'> <br>" );
-			 output.println( "<INPUT TYPE=submit VALUE='Submit'>" );
+		  output.println( "");
+		  output.println( " ");
+		  output.println( " ");
+		  output.println( " ");
+		  output.println( " ");
+		  output.println( " ");
+		  output.println( " ");
+		  output.println( " ");
 	  }
 	  if(category.equals("console")){
 		  output.println( "<h1 style='font-family: TimesNewRoman; color: red;'>Please select consoles you are interested in</h1>");		  
@@ -92,7 +129,6 @@ public class formB extends HttpServlet {
 
 	  }
 		output.close();    // close stream
-
+*/
    }
-
 }
