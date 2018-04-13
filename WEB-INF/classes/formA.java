762e3d337764206210f2b7b72ecdaa26cac63c3a
aristotelis96 <aristotelis96@users.noreply.github.com>
2018-04-13 13:03:39 +0300

    images are now stored under ./images folder

--- WEB-INF/classes/formA.java
+++ WEB-INF/classes/formA.java
@@ -20,7 +20,7 @@ public class formA extends HttpServlet {
 	  output.println( "font-size:      120px;" );
 	  output.println( "font-style:    italic; color: red;background-color : black; text-align: center;" );
 	  output.println( "}.form-radio-item label, .form-checkbox-item label {white-space: normal; display: block; left: 14px; top: -14px; position relative; } </style>" );
-	  output.println( "<BODY text='white' background='./backgroundIntro.png'></HEAD><h1 class='header' text-align='center'>Let's begin!</h1><br><div align='center'>" );
+	  output.println( "<BODY text='white' background='./images/backgroundIntro.png'></HEAD><h1 class='header' text-align='center'>Let's begin!</h1><br><div align='center'>" );
 	  output.println( "<FORM ACTION='./formB' method='get'>" );
 	  output.println( "<h1 style='font-family: TimesNewRoman; color: Green'>First give us some info!</h1>" );
 	  output.println( "First name:<br>" );
@@ -32,9 +32,9 @@ public class formA extends HttpServlet {
 	  output.println( "E-mail:<br>" );
 	  output.println( "<input type='text' name='mail'><br><br><br>" );
 	  output.println( "<h1 style='font-family: TimesNewRoman; color: Green'>Now select a category!</h1>" );
-	  output.println( "<label><img style=' width: 200px; height: 150px' src='./console.png'>Consoles<input style='margin-right: 50px;' type='radio' name='category' value='console' checked='checked' /></label>" );
-	  output.println( "<label><img style='width: 250px; height: 150px' src='./pc.jpg'>Pc Components<input style='margin-right: 50px;' type='radio' name='category' value='pc' /></label>" );
-	  output.println( "<label><img style='width: 250px; height: 150px' src='./gameslibrary.png'>Games<input style='margin-right: 50px;' type='radio' name='category' value='Games' /></label>" );
+	  output.println( "<label><img style=' width: 200px; height: 150px' src='./images/console.png'>Consoles<input style='margin-right: 50px;' type='radio' name='category' value='console' checked='checked' /></label>" );
+	  output.println( "<label><img style='width: 250px; height: 150px' src='./images/pc.jpg'>Pc Components<input style='margin-right: 50px;' type='radio' name='category' value='pc' /></label>" );
+	  output.println( "<label><img style='width: 250px; height: 150px' src='./images/gameslibrary.png'>Games<input style='margin-right: 50px;' type='radio' name='category' value='Games' /></label>" );
 	  output.println( "<br><br><INPUT TYPE=submit VALUE='Submit'></FORM></div></BODY></HTML>" );
       output.close();    // close stream
    }
